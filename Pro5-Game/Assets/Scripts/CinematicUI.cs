ecca041dd19e820b09771ae0fbcaa1d6e4ec6d2c
Florian Rakos <58404248+FlorianRakos@users.noreply.github.com>
2023-02-03 01:09:09 +0100

    Cinematic UI, Letter

---
+++ Pro5-Game/Assets/Scripts/CinematicUI.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Whilefun.FPEKit;
+
+public class CinematicUI : MonoBehaviour
+{
+    [SerializeField] private GameObject UI;
+    
+    private FPEInteractionManagerScript im;
+    private bool uiActive = false;
+    
+    
+    void Start()
+    {
+        im = FindObjectOfType<FPEInteractionManagerScript>();
+        UI.SetActive(false);
+    }
+
+    public void startCinematic()
+    {
+        uiActive = true;
+        UI.SetActive(true);
+        im.BeginCutscene(true);
+    }
+
+    public void stopCinematic()
+    {
+        uiActive = false;
+        UI.SetActive(false);
+        im.EndCutscene(true);
+    }
+
+    
+    
+}
