884cda5ab6eb52211160a824fde4497141c56a43
Florian Rakos <58404248+FlorianRakos@users.noreply.github.com>
2023-02-06 02:20:27 +0100

    Clock improved, UI fixed, Riddle Helper Particles

--- Pro5-Game/Assets/Scripts/InGameScripts/Interaction/Clock.cs
+++ Pro5-Game/Assets/Scripts/InGameScripts/Interaction/Clock.cs
@@ -8,43 +8,52 @@ public class Clock : MonoBehaviour
 
     [SerializeField] private FPEDrawer drawer;
     [SerializeField] AudioClip drawerOpen;
-    private float rotationGoal = -240.0f;    
+    private float rotationGoal = -540.0f;    
     [SerializeField] GameObject pointerBase;
     [SerializeField] GameObject hourPointerBase;
-    private float rotationPerAction = 30.0f;
+    private float rotationPerAction = 90.0f;
     private float currentRotation = 0.0f;
+    private bool drawerOpened;
+
+    private AudioSource audioSource;
     
     
-    // public void Awake()
-    // {
-    //     pointerBase = GameObject.Find("PointerBase");
-    //     hourPointerBase = GameObject.Find("HourPointerBase");
-    // }
+    public void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
 
     public void TurnClock(bool turnForward)
     {
         if (turnForward)
         {
-            print("Turn forward");
             pointerBase.transform.Rotate(0, 0,rotationPerAction);
             hourPointerBase.transform.Rotate(0, 0,rotationPerAction / 12f);
             currentRotation += rotationPerAction;
         }
         else
         {
-            print("Turn backward");
             pointerBase.transform.Rotate(0, 0,-rotationPerAction);
             hourPointerBase.transform.Rotate(0, 0,-rotationPerAction / 12f);
             currentRotation -= rotationPerAction;
         }
-        
 
+        if (drawerOpened) return;
 
         float diff = rotationGoal - currentRotation;
-        if ( diff < 0.1f && diff > -0.1f)
+        
+        // 4320 is 12 hours rotation
+        if ((diff < 0.1f && diff > -0.1f) || diff < (-4319))
         {
+            drawerOpened = true;
             drawer.ExternallyUnlockDrawer();
             drawer.openTheDrawer();
+            audioSource.PlayOneShot(drawerOpen);
+
+            //GameObject cam = GameObject.Find("FPEPlayerController(Clone)");
+            FPEFirstPersonController controller = FindObjectOfType<FPEFirstPersonController>();
+            controller.forcePlayerLookToPosition(drawer.transform.position);
+            //controller.transform.LookAt(drawer.transform);
         }
     }
 }
